1488d33307bdb004077eb507a0e2e021382e2c7e
protivotankovaiaystanovka <navamelim.gmail.com>
2025-01-27 14:53:29 +0900

    initial

---
+++ zzzadanie1-4.js
@@ -0,0 +1,31 @@
+const myName = "Владислава"
+const realnost = "не"
+const programmingLanguage = "JavaScript"
+const curseCreatorName = "самогокрутогопреводавателяНикиты Михайловича"
+const reasonText = "хочу зарабатывать монога деняк и купить виллу на мальдивах"
+const numberOfMonth = "0"
+
+const myinfoText = `всем привет! меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${curseCreatorName}
+Я собираюсь стать веб-разработчиком, потому что ${reasonText}. До этого я изучала ${programmingLanguage} ${numberOfMonth} месяцев. Я уверена что ${realnost} пройду данный курс до конца`
+
+// console.log(myinfoText)
+
+// console.log(myinfoText.replaceAll(programmingLanguage, programmingLanguage.toUpperCase()))
+
+// const zov = myName.length; 
+// console.log("Первый символ:", myinfoText[0]); 
+// console.log("Последний символ:", myinfoText[zov - 1]); 
+
+
+// const userAnswer = prompt('Как vас zovут?')
+// const userAge = prompt("Сколько vам лет?")
+// const noSpaces = userAnswer.toLowerCase().trim(); 
+// console.log(noSpaces); 
+// alert("Вас зовут " + noSpaces + " и вам " + userAge + " лет");
+
+// let userString = prompt("Введите текст для обрезки").trim()
+// let startSliceIndex = prompt("Введите индекс, с которого нужно начать обрезку строки")
+// let endSliceIndex = prompt("Введите индекс, которым нужно закончить обрезку строки")
+// let resultString = userString.slice(startSliceIndex, endSliceIndex)
+// alert(resultString)
+
---
+++ zzzadanie5.js
@@ -0,0 +1,11 @@
+/* let userText = prompt("Введите текст:").trim()
+let wordFromText = prompt("Введите слово из текста: ").trim()
+let indexOfWord = userText.indexOf(wordFromText)
+string = userText.substring(0, indexOfWord)
+alert(string) */
+
+
+let text = 'Nikita'
+let middleSym = text.length / 2
+
+console.log(text[middleSym])
